dae455474e9784ee0af3de672648e4e8e1fc8597
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2021-02-22 17:39:25 -0500

    interface pour représenter les graphes

---
+++ src/Igraph.java
@@ -0,0 +1,5 @@
+public interface Igraph {
+    void addNode(Node node);
+    void addEdge(Node debut, Node fin);
+
+}
---
+++ src/Node.java
@@ -0,0 +1,7 @@
+public class Node {
+    private String name;
+
+    public Node(String name){
+        this.name = name;
+    }
+}
